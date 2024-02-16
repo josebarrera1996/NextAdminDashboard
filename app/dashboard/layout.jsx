56115c5f1c740e3cb567355b906d2bc2d03cf9c7
José Barrera <josebarrerag1996@gmail.com>
2024-02-16 11:43:30 -0300

    Sidebar Design

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,14 +1,15 @@
 import Navbar from "../ui/dashboard/navbar/navbar"
 import Sidebar from "../ui/dashboard/sidebar/sidebar"
+import styles from '../ui/dashboard/dashboard.module.css';
 
 // Layout para el contenido dentro del 'Dashboard'
 const Layout = ({ children }) => {
     return (
-        <div>
-            <div>
+        <div className={styles.container}>
+            <div className={styles.menu}>
                 <Sidebar />
             </div>
-            <div>
+            <div className={styles.content}>
                 <Navbar />
                 {children}
             </div>
