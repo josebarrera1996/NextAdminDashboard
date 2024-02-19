2ac3cf617a733d14a652ea1bbcbbc077292c9670
José Barrera <josebarrerag1996@gmail.com>
2024-02-19 11:23:06 -0300

    Navbar Design

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -99,6 +99,10 @@ const Sidebar = () => {
           </li>
         ))}
       </ul>
+      <button className={styles.logout}>
+        <MdLogout />
+        Logout
+      </button>
     </div>
   )
 }
