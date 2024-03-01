487562459ddc8e9c272d7d22b3150c04237e4db7
José Barrera <josebarrerag1996@gmail.com>
2024-03-01 12:51:44 -0300

    Data Fetching

---
+++ app/lib/data.js
@@ -0,0 +1,34 @@
+import { User } from "./models"
+import { connectToDB } from "./utils";
+
+export const fetchUsers = async () => {
+    try {
+        connectToDB();
+        const users = await User.find()
+        return users
+    } catch (error) {
+        console.log(error)
+        throw new Error("Failed to fetch users!")
+    }
+}
+
+export const cards = [
+    {
+        id: 1,
+        title: "Total Users",
+        number: 10.928,
+        change: 12,
+    },
+    {
+        id: 2,
+        title: "Stock",
+        number: 8.236,
+        change: -2,
+    },
+    {
+        id: 3,
+        title: "Revenue",
+        number: 6.642,
+        change: 18,
+    },
+];
